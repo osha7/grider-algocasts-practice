4b1701a135a0d662f47043f12a5b3083ffa1b505
osha7 <ogroetz@gmail.com>
2020-09-15 22:02:30 -0600

    reverseint completed

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,26 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    // debugger
+    // Math.sign(n) can be only 1 of 2 diff values 1 or -1 depend. on sign of n (positive or negative)
+    return Math.sign(n)*parseInt(n.toString().split('').reverse().join(''))
+}
+
+// reverseInt(189)
+
+// function reverseInt(n) {
+//     const reversed = n.toString().split('').reverse().join('')
+//     if (n < 0) {
+//         return parseInt(reversed) * -1
+//     } 
+//     return parseInt(reversed)
+// }
+
+// function reverseInt(n) {
+//     const reversed = n.toString().split('').reverse().join('')
+//   return parseInt(reversed) * Math.sign(n)
+// }
+
 
 module.exports = reverseInt;
