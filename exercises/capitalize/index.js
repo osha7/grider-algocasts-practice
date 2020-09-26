7ecdf48c2d26d433c9ed8a892683785d1efac18c
osha7 <ogroetz@gmail.com>
2020-09-26 16:33:10 -0600

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,44 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// function capitalize(str) {
+//     // debugger
+//     let newArray = []
+  
+//     let array1 = str.split(' ')
+    
+//     for (let i = 0; i < array1.length; i++) {
+//         firstLetter = array1[i].charAt(0).toUpperCase()
+//         newString = array1[i].slice(1)
+//         newArray.push(firstLetter + newString)
+//     }
+//     return newArray.join(' ')
+    
+// }
 
+// function capitalize(str) {
+
+//     const words = []
+    
+//     for (let word of str.split(' ')) {
+//         words.push(word[0].toUpperCase() + word.slice(1))
+//     }
+//     return words.join(' ')
+    
+// }
+
+function capitalize(str) {
+    let result = str[0].toUpperCase()
+    for (let i = 1; i < str.length; i++) {
+        if (str[i - 1] === ' ') {
+            result += str[i].toUpperCase()
+        } else {
+            result += str[i]
+        }
+    }
+    return result
+}
+
+
+// capitalize('a short sentence')
 module.exports = capitalize;
